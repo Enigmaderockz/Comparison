f1e46bafdbbae0ba70ef97d19bde19c51fedc2ea
Abhinandan Mehandiratta <39618909+Enigmaderockz@users.noreply.github.com>
2023-07-16 16:51:21 +0530

    Update recon_perfromance_updated.py

--- recon_perfromance_updated.py
+++ recon_perfromance_updated.py
@@ -8,19 +8,22 @@ def perform_recon_on_files(file1, file2, col_check=None, column_mapping=None):
         extra_records2 = []
 
         # Read the CSV files
-        with open(file1, 'r') as f1, open(file2, 'r') as f2:
+        with open(file1, "r") as f1, open(file2, "r") as f2:
             csv_reader1 = csv.DictReader(f1, delimiter=delimiter)
             csv_reader2 = csv.DictReader(f2, delimiter=delimiter)
             rows1 = list(csv_reader1)
             rows2 = list(csv_reader2)
 
-        col_check_list = col_check.split(',')
+        col_check_list = col_check.split(",")
 
         if column_mapping is not None:
-            col_check_list_mapped = [column_mapping.get(col, col) for col in col_check_list]
+            col_check_list_mapped = []
+            for col in col_check_list:
+                mapped_col = column_mapping.get(col, col)
+                col_check_list_mapped.append(mapped_col)
             col_check_list = col_check_list_mapped
 
-        def find_extra_records(row1):
+        def find_extra_records_file1(row1):
             found = False
             for row2 in rows2:
                 match = True
@@ -33,8 +36,8 @@ def perform_recon_on_files(file1, file2, col_check=None, column_mapping=None):
                     break
             if not found:
                 return row1
-        
-        def find_extra_records1(row2):
+
+        def find_extra_records_file2(row2):
             found = False
             for row1 in rows1:
                 match = True
@@ -49,10 +52,10 @@ def perform_recon_on_files(file1, file2, col_check=None, column_mapping=None):
                 return row2
 
         with concurrent.futures.ThreadPoolExecutor() as executor:
-            extra_records1 = list(executor.map(find_extra_records, rows1))
+            extra_records1 = list(executor.map(find_extra_records_file1, rows1))
 
         with concurrent.futures.ThreadPoolExecutor() as executor:
-            extra_records2 = list(executor.map(find_extra_records1, rows2))
+            extra_records2 = list(executor.map(find_extra_records_file2, rows2))
 
         extra_records1 = [record for record in extra_records1 if record is not None]
         extra_records2 = [record for record in extra_records2 if record is not None]
@@ -67,39 +70,64 @@ def perform_recon_on_files(file1, file2, col_check=None, column_mapping=None):
             rows2.remove(row)
 
         # Write the results to output.csv
-        with open('output.csv', 'w', newline='') as output_file:
+        with open("output.csv", "w", newline="") as output_file:
             writer = csv.writer(output_file, delimiter=delimiter)
-            
-            writer.writerow([
-                f'Extra records in {file1} which are not present in {file2} based on ' + col_check + ': ' + str(len(extra_records1))
-            ])
+
+            writer.writerow(
+                [
+                    f"Extra records in {file1} which are not present in {file2} based on "
+                    + col_check
+                    + ": "
+                    + str(len(extra_records1))
+                ]
+            )
             writer.writerow([])
             if extra_records1:
                 writer.writerow(csv_reader1.fieldnames)
                 for row in extra_records1:
-                    writer.writerow([str(val).replace("", "") if val is not None else '' for val in row.values()])
+                    writer.writerow(
+                        [
+                            str(val).replace("", "") if val is not None else ""
+                            for val in row.values()
+                        ]
+                    )
             else:
                 writer.writerow([])
 
             writer.writerow([])
 
             # Write the second set of extra records
-            writer.writerow([
-                f'Extra records in {file2} which are not present in {file1} based on ' + col_check + ': ' + str(len(extra_records2))
-            ])
+            writer.writerow(
+                [
+                    f"Extra records in {file2} which are not present in {file1} based on "
+                    + col_check
+                    + ": "
+                    + str(len(extra_records2))
+                ]
+            )
             writer.writerow([])
             if extra_records2:
                 writer.writerow(csv_reader2.fieldnames)
                 for row in extra_records2:
-                    writer.writerow([str(val).replace("", "") if val is not None else '' for val in row.values()])
+                    writer.writerow(
+                        [
+                            str(val).replace("", "") if val is not None else ""
+                            for val in row.values()
+                        ]
+                    )
             else:
                 writer.writerow([])
 
         # Save the updated temporary files
-        with open(file1, 'w', newline='') as f1, open(file2, 'w', newline='') as f2:
-            writer1 = csv.DictWriter(f1, fieldnames=csv_reader1.fieldnames, delimiter=delimiter)
-            writer2 = csv.DictWriter(f2, fieldnames=csv_reader2.fieldnames, delimiter=delimiter)
+        with open(file1, "w", newline="") as f1, open(file2, "w", newline="") as f2:
+            writer1 = csv.DictWriter(
+                f1, fieldnames=csv_reader1.fieldnames, delimiter=delimiter
+            )
+            writer2 = csv.DictWriter(
+                f2, fieldnames=csv_reader2.fieldnames, delimiter=delimiter
+            )
             writer1.writeheader()
             writer2.writeheader()
             writer1.writerows(rows1)
             writer2.writerows(rows2)
+
